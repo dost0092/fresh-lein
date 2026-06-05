12616de6beb033d734dd3f79568a5477a6edc91f
Waqas Dost <waqas@kruiz.co>
2026-06-05 09:59:51 +0500

    ANALYTICS ADDED

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/react';
 import { Toaster } from '@/components/ui/toaster';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { queryClientInstance } from '@/lib/query-client';
@@ -132,6 +133,7 @@ function App() {
         <Router>
           <AppRoutes />
         </Router>
+        <Analytics />
         <Toaster />
       </QueryClientProvider>
     </AuthProvider>
